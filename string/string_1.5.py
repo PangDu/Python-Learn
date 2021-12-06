5205260d25208806a0e0dc280e4fe7298030b2bb
PangDu <792229197@qq.com>
2021-12-06 16:05:12 +0800

    add new function

--- string/string_1.5.py
+++ string/string_1.5.py
@@ -1,3 +1,4 @@
+# - * - coding: UTF-8 - * -
 import string
 """
     案例二十三 转换字母大小写
@@ -49,4 +50,28 @@ indexRight = src_str.rfind('月')
 print(f'原字符串:{src_str}')
 print(f'从左向右查找,"月"字的位置为: {indexLeft}.')
 print(f'从右向左查找,"月"字的位置为: {indexRight}.')
+print("="*36)
+
+"""
+    案例二十七 startswith 与 endswitch 方法
+
+"""
+print(f'{"startswith 与 endswitch 方法" :^30s}' + "\n")
+stringOne = "capthion"
+print(f'字符串{stringOne}是否以"ca"开头?\n{stringOne.startswith("ca")}')
+stringTwo = "panda"
+print(f'字符串{stringTwo}是否以"de"结尾?\n{stringTwo.endswith("de")}')
+print("="*36)
+
+"""
+    案例二十八 统计子字符串出现的次数
+
+"""
+print(f'{"统计子字符串出现的次数" :^30s}' + "\n")
+stringOne = "awedfgtrqaasd"
+n = stringOne.count('a',0,9)
+print(f'"a"在字符串 {stringOne} 出现的次数为{n}次.')
+stringOne = "一生一次一双人"
+n = stringOne.count("一")
+print(f'"一"在字符串{stringOne}中出现的次数为{n}次.')
 print("="*36)
